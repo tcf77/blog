6b92658c0fe0e01e136786baef6e1086ff8093d5
JoeTj <joekic@hotmail.com>
2016-05-19 10:26:13 -0400

    changed arvixe publish profile setting

---
+++ TCF/DbConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCF
+{
+    public class DbConnection
+    {
+        public DbConnection(string connectionName)
+        {
+            
+        }
+    }
+}
